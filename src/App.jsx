3f0cac3a873083ad71b8893dc7a683ac503b3591
ramaherdianto <ramadhanherdianto35@gmail.com>
2022-12-29 19:19:10 +0700

    feat: update components

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,9 @@
-import Axios from 'axios';
-import { useEffect, useState } from 'react';
-import DisplayListMovie from './components/DisplayListMovie';
-import FilterMovies from './components/FilterMovies';
-import SearchMovies from './components/SearchMovies';
-import { baseURL, apiKEY } from './api/Api';
+import MainContent from './components/MainContent';
 
 function App() {
-    const [movies, setMovies] = useState({
-        results: [],
-        filterType: '',
-        movieDetails: '',
-        alerMsgModal: '',
-    });
-
-    // Get data from API
-    const showMoviesOnBtn = (e) => {
-        const buttonType = typeof e === 'string' ? e : e.target.className;
-        Axios(`${baseURL}/movie/${buttonType}?api_key=${apiKEY}&page=1`)
-            .then((data) => {
-                let results = data.data.results;
-                setMovies((prevState) => {
-                    return { ...prevState, results: results, filterType: buttonType };
-                });
-            })
-            .catch((err) => console.info('Error', err.message));
-    };
-
-    useEffect(() => {
-        showMoviesOnBtn('popular');
-    }, []);
     return (
         <div className='App'>
-            <header>
-                <SearchMovies />
-            </header>
-            <main className='flex flex-nowrap'>
-                <aside className='flex min-w-[80px] min-h-screen bg-sidebar justify-center drop-shadow-2xl'>
-                    <FilterMovies showMoviesOnBtn={showMoviesOnBtn} />
-                </aside>
-                <section className='bg-[#24323F] flex justify-center'>
-                    <DisplayListMovie results={movies.results} filterType={movies.filterType} />
-                </section>
-            </main>
+            <MainContent />
         </div>
     );
 }
